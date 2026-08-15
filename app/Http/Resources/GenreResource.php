f2546f0afd5a66c5c778fba30ea561d2af62b3f4
toshiaki_taniguchi <ta2gucity@gb3.so-net.ne.jp>
2026-08-15 17:15:51 +0900

    feat: レスポンス整形用の API Resource クラス（Book, Genre, Review）を作成

---
+++ app/Http/Resources/GenreResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GenreResource extends JsonResource
+{
+    /**
+     * リソースを配列に変換します。
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+        ];
+    }
+}
